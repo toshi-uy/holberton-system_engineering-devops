5aeda75a2f00c2dcc8b5df0b79bc43b023b5ad7e
toshi-uy <s_borgia@hotmail.com>
2021-06-21 21:30:59 -0300

    changed return for exit

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -29,31 +29,31 @@ int main(void)
 	if (z1 > 0)
 		printf("Zombie process created, PID: %d\n", z1);
 	else
-		return (1);
+		exit(1);
 
 	z2 = fork();
 	if (z2 > 0)
 		printf("Zombie process created, PID: %d\n", z2);
 	else
-		return (1);
+		exit(1);
 
 	z3 = fork();
 	if (z3 > 0)
 		printf("Zombie process created, PID: %d\n", z3);
 	else
-		return (1);
+		exit(1);
 
 	z4 = fork();
 	if (z4 > 0)
 		printf("Zombie process created, PID: %d\n", z4);
 	else
-		return (1);
+		exit(1);
 
 	z5 = fork();
 	if (z5 > 0)
 		printf("Zombie process created, PID: %d\n", z5);
 	else
-		return (1);
+		exit(1);
 
 	infinite_while();
 }
