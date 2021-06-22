15d327b4885369aab7c940cdd65d2e6e3281d3f1
toshi-uy <s_borgia@hotmail.com>
2021-06-21 21:27:41 -0300

    added betty style

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -5,7 +5,7 @@
  * infinite_while - infinite loop
  * Return: 0
  */
- 
+
 int infinite_while(void)
 {
 	while (1)
@@ -25,35 +25,35 @@ int main(void)
 {
 	pid_t z1, z2, z3, z4, z5;
 
-    z1 = fork();
-    if (z1 > 0)
-        printf("Zombie process created, PID: %d\n", z1);
-    else
-        return (1);
+	z1 = fork();
+	if (z1 > 0)
+		printf("Zombie process created, PID: %d\n", z1);
+	else
+		return (1);
 
-    z2 = fork();
-    if (z2 > 0)
-        printf("Zombie process created, PID: %d\n", z2);
-    else
-        return (1);
+	z2 = fork();
+	if (z2 > 0)
+		printf("Zombie process created, PID: %d\n", z2);
+	else
+		return (1);
 
-    z3 = fork();
-    if (z3 > 0)
-        printf("Zombie process created, PID: %d\n", z3);
-    else
-        return (1);
+	z3 = fork();
+	if (z3 > 0)
+		printf("Zombie process created, PID: %d\n", z3);
+	else
+		return (1);
 
-    z4 = fork();
-    if (z4 > 0)
-        printf("Zombie process created, PID: %d\n", z4);
-    else
-        return (1);
+	z4 = fork();
+	if (z4 > 0)
+		printf("Zombie process created, PID: %d\n", z4);
+	else
+		return (1);
 
-    z5 = fork();
-    if (z5 > 0)
-        printf("Zombie process created, PID: %d\n", z5);
-    else
-        return (1);
+	z5 = fork();
+	if (z5 > 0)
+		printf("Zombie process created, PID: %d\n", z5);
+	else
+		return (1);
 
 	infinite_while();
 }
