2242f250365bf0802d70735cce6c56d7a7bb0e63
toshi-uy <s_borgia@hotmail.com>
2021-08-13 13:43:17 -0300

    added provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #Using Puppet, install puppet-lint
-package { 'gem install puppet-lint':
-          require => '-v 2.1.1',
-          ensure  => ['2.1.1',
-                      'created'],
+package { 'puppet-lint':
+          ensure    => ['2.1.1',
+                        'created'],
+          provider  => 'gem',
       }
