5ef4d278575db3124255d6ea76e5d97fba32bb8b
toshi-uy <s_borgia@hotmail.com>
2021-08-13 14:06:17 -0300

    arranged indentation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #Using Puppet, install puppet-lint
 package { 'puppet-lint':
-          ensure    => ['2.1.1',
+          ensure   => ['2.1.1',
                         'created'],
-          provider  => 'gem',
+          provider => 'gem',
       }
