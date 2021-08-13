03efca1bb8ff34c2175cf95b98fda11d161e0c2f
toshi-uy <s_borgia@hotmail.com>
2021-08-13 13:35:39 -0300

    added require

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #Using Puppet, create a file in /tmp
 package { 'puppet-lint':
-          ensure => ['2.1.1',
+          ensure  => ['2.1.1',
                     'created']
+          require => Package['openssl']
       }
