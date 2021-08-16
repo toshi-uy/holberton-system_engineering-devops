cb65c32ce922b0bdf8f5579de210e6163c598cfd
toshi-uy <s_borgia@hotmail.com>
2021-08-15 23:56:57 -0300

    added ensure

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # changes to our configuration file
 file_line {
-    path  => '/etc/ssh/ssh_config',
-    line  => 'PasswordAuthentication no',
-    match => 'PasswordAuthentication yes',
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+    match  => 'PasswordAuthentication yes',
 }
 file_line {
-    path  => '/etc/ssh/ssh_config',
-    line  => 'IdentityFile ~/ssh/holberton',
-    match => 'IdentityFile ~/ssh/id_rsa',
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/ssh/holberton',
+    match  => 'IdentityFile ~/ssh/id_rsa',
 }
