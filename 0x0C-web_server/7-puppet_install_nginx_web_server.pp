d5dbd7f66397d473c869dc2aa4be39e23d0ac718
toshi-uy <s_borgia@hotmail.com>
2021-08-17 17:31:21 -0300

    added and testing service

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# install and configure an Nginx server using Puppet instead of Bash
+
+package { 'nginx':
+    ensure => present,
+}
+
+file { 'index.html':
+    ensure  => present,
+    path    => '/var/www/html/index.html',
+    content => 'Holberton School\n',
+}
+
+file_line { 'redirect':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server;',
+    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect'],
+}
