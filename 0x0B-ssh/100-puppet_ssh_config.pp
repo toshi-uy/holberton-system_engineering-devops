45b0b66cc22b3be3c6d65d453976b56052d731c8
toshi-uy <s_borgia@hotmail.com>
2021-08-21 18:28:09 -0300

    added ~

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # changes to our configuration file
 
 file_line { 'no_password':
-    path   => '/etc/ssh/ssh_config',
+    path   => '~/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/ssh/holberton',
 }
 file_line { 'change_key':
-    path   => '/etc/ssh/ssh_config',
+    path   => '~/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
 }
