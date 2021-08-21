491e95a41776a4699a5be57ffeb93bd995b7cb78
toshi-uy <s_borgia@hotmail.com>
2021-08-21 18:27:12 -0300

    removed ensure

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 # changes to our configuration file
+
 file_line { 'no_password':
-    ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/ssh/holberton',
 }
 file_line { 'change_key':
-    ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
 }
