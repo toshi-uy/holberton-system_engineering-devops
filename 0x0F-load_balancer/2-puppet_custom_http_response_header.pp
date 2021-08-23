2de3b94f1ff3ba6692bfe97295a8d889f701299c
toshi-uy <s_borgia@hotmail.com>
2021-08-23 19:15:19 -0300

    changed after

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,15 @@
 # automate the task of creating a custom HTTP header response, but with Puppet
 
 package { 'nginx':
-    ensure => present,
-    name   => 'nginx',
+  ensure => present,
+  name   => 'nginx',
 }
 
-file_line { 'header':
-    path    => '/etc/nginx/sites-available/default',
-    after => 'root /var/www/html;',
-    line    => "add_header X-Served-By ${hostname};",
-    require => Package['nginx'],
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'add_header X-Served-By "$HOSTNAME";',
 }
 
 exec { 'restart':
