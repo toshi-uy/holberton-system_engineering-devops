fc7f9bf4fc20a3b0ad329ba89b41b824e0a1a987
toshi-uy <s_borgia@hotmail.com>
2021-09-13 17:20:31 -0300

    testing json

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,6 +4,7 @@ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
+
 if __name__ == "__main__":
     import requests
     from sys import argv
