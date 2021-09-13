c2e090b7d3cb405838e4898e919f296b2f8652ac
toshi-uy <s_borgia@hotmail.com>
2021-09-13 17:18:40 -0300

    testing json

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
+
     if type(argv[1]) is int:
-        req_users = requests.get('https://jsonplaceholder.typicode.com/users')
-        req_todo_list = requests.get('https://jsonplaceholder.typicode.com/todos')
+        req_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+        req_todo_list = requests.get('https://jsonplaceholder.typicode.com/{}/todos'.format(argv[1]))
         todo_list = req_todo_list.json
         users = req_users.json
-        print("[{}] {}".format(users.get('id'), users.get('name')))
+        print("[{}]".format(users.get('name')))
