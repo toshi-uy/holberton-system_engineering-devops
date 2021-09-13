c59ec7b292cfdf524d2d0e9810eeecab18e6a564
toshi-uy <s_borgia@hotmail.com>
2021-09-13 19:49:46 -0300

    changed print

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,12 +3,11 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
-
     id = argv[1]
     req_users = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(id))
