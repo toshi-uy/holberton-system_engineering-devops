52c15b43143cb1e4d4e221a903f706b3783bed7f
toshi-uy <s_borgia@hotmail.com>
2021-09-28 16:05:05 -0300

    added print to debug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,7 +18,7 @@ def top_ten(subreddit):
     if req_hot.status_code == 200:
         hot = req_hot.json().get('data').get('children')
         for data in hot:
+            print(data)
             title = data.get('data').get('title')
-            title.decode('ascii')
             print(title)
     print(None)
