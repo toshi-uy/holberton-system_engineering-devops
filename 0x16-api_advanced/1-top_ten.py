c4580047f0d62e510c8e7dc2ff26eeb32d2beae6
toshi-uy <s_borgia@hotmail.com>
2021-09-28 15:59:38 -0300

    enconer

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -19,5 +19,6 @@ def top_ten(subreddit):
         hot = req_hot.json().get('data').get('children')
         for data in hot:
             title = data.get('data').get('title')
+            title.encode('ascii', 'ignore')
             print(title)
     print(None)
