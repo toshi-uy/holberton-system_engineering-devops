62bf9384439950bbe3f35a5870302a4c906630a0
toshi-uy <s_borgia@hotmail.com>
2021-09-28 15:25:25 -0300

    added headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
     """
     returns the number of subribers of a subreddit or 0 if fails
     """
-    header = {'User-Agent': 'android:com.example.myredditapp:v1.2.3 (by /u/kemitche)'}
-    req_users = requests.get('https://www.reddit.com/r/' + subreddit + '/mine/subscribers.json', allow_redirects=False)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    req_users = requests.get('https://www.reddit.com/r/' + subreddit + '/mine/subscribers.json', headers=headers, allow_redirects=False)
     if req_users.status_code == 404:
         return 0
     users = req_users.json()
