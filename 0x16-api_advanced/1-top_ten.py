ef29b75bd212f047ecf062c070fc4093fc1f4ccd
toshi-uy <s_borgia@hotmail.com>
2021-09-28 16:01:57 -0300

    testing encoder

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,11 +13,12 @@ def top_ten(subreddit):
     """
     headers = {"User-Agent": "Mozilla/5.0"}
     req_hot = requests.get('https://www.reddit.com/r/' +
-                             subreddit + '/hot.json?limit=10', charset='utf-8',
+                             subreddit + '/hot.json?limit=10',
                              headers=headers, allow_redirects=False)
     if req_hot.status_code == 200:
         hot = req_hot.json().get('data').get('children')
         for data in hot:
             title = data.get('data').get('title')
+            title.encode('UTF-8'))
             print(title)
     print(None)
