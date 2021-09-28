5737c4e0d9fbafbfae8410c2a22d092b445de210
toshi-uy <s_borgia@hotmail.com>
2021-09-28 15:53:06 -0300

    testing prints

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,5 +18,5 @@ def top_ten(subreddit):
     if req_hot.status_code == 200:
         hot = req_hot.json().get('data').get('children')
         for title in hot:
-            print (title.get('data').get('title'))
-    print("None")
+            print(title.get('data').get('title'))
+    print(None)
