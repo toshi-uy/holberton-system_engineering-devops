6f61e9e4673f1444a452c7700ede647b2b86d1d0
toshi-uy <s_borgia@hotmail.com>
2021-09-28 15:27:03 -0300

    subscriber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,6 +13,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     req_users = requests.get('https://www.reddit.com/r/' + subreddit + '/mine/subscriber.json', headers=headers, allow_redirects=False)
     if req_users.status_code == 404:
+        print("error")
         return 0
     users = req_users.json()
     print(users)
