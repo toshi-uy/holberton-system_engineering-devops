27e83b0c8d7f4ef549bb40a184e6117114b2c5ea
toshi-uy <s_borgia@hotmail.com>
2021-09-28 15:50:41 -0300

    testing prints

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts listed for a given
+    subreddit or None if fails
+    """
+    headers = {"User-Agent": "Mozilla/5.0"}
+    req_hot = requests.get('https://www.reddit.com/r/' +
+                             subreddit + '/hot?limit=10.json',
+                             headers=headers, allow_redirects=False)
+    if req_hot.status_code == 200:
+        hot = req_hot.json().get('data').get('children')
+        for title in hot:
+            print (title.get('data').get('title'))
+    print("None")
