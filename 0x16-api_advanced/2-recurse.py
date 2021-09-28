a8fec40c3e2e5892eedc24c5094af3746fad602d
toshi-uy <s_borgia@hotmail.com>
2021-09-28 17:19:46 -0300

    added recursion working

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], pagination=""):
+    """
+    queries the Reddit API and returns a list containing the titles
+    of all hot articles for a given subreddit or None
+    """
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
+    req_hot = requests.get(url, headers=headers, after=pagination,
+                           allow_redirects=False)
+    if req_hot.status_code != 200:
+        return None
+    response = req_hot.json()
+    hot = response.get('data').get('children')
+    for data in hot:
+        hot_list.append(data.get('data').get('title'))
+        pagination = response.get('data').get('after')
+        if pagination is not None:
+            recurse(subreddit, hot_list, pagination)
+    return hot_list
