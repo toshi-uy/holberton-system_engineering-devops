c0cba10575773a1664225eb39eb5d4796f77d29c
toshi-uy <s_borgia@hotmail.com>
2021-09-29 16:24:19 -0300

    added /

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Puppet Code to fix the error
 exec{'fix-wordpress':
-    command => "sed -i 's/phpp/php' /var/www/html/wp-settings.php",
+    command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
     path    => "/usr/bin:/bin",
 }
