e153bd6489c0833532eefb426fec89030808c2a4
toshi-uy <s_borgia@hotmail.com>
2021-09-29 19:35:25 -0300

    testing count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -16,23 +16,26 @@ def count_words(subreddit, word_list, pagination="", results={}, count=0):
     headers = {"User-Agent": "Mozilla/5.0"}
     url = 'https://www.reddit.com/r/' + subreddit +\
           '/hot.json?after={}'.format(pagination)
-    req_hot = requests.get(url, headers=headers, allow_redirects=False)
+    req_hot = requests.get(url, headers=headers, 
+                           allow_redirects=False, count=count, limit=100)
     if req_hot.status_code != 200:
         return
     else:
         response = req_hot.json()
+        count += results.get('data').get('dist')
         hot = response.get('data').get('children')
         for data in hot:
             title = data.get('data').get('title')
-            for i in word_list:
-                for j in title.split():
-                    if i.lower() in j.lower():
-                        count += 1
-                results[i] = count
-            count = 0
+            for word in word_list:
+                if word in title.split():
+                    times = len([t for t in title.split() if t == word.lower()])
+                    if results.get(word) is None:
+                        results[word] = times
+                    else:
+                        results[word] += times
         pagination = response.get('data').get('after')
         if pagination is not None:
             count_words(subreddit, word_list, pagination, results, count)
         else:
-            for key, value in results.items():
+            for key, value in sorted(results.items()):
                 print("{}: {}".format(key, value))
