6dc70b9d1fbd2ee2d85bac67a7ab054c65bc9e04
toshi-uy <s_borgia@hotmail.com>
2021-10-07 18:54:07 -0300

    added spaces to puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Puppet Code to fix the error
 exec { 'fix_ngnix':
   command => 'sed -i s/15/4096/ /etc/default/nginx',
-  path => '/usr/bin:/bin',
+  path    => '/usr/bin:/bin',
 }
 exec { 'restart_nginx':
   command => 'sudo service nginx restart',
